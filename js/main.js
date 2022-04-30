96074cffd4950f2c1705d48f799b0fc8986a04c3
j-imsa <cse.isalehi@gmail.com>
2022-04-30 14:06:36 +0430

    create a basic request

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-const HeaderOnClickedHandler = (event) => {
-    console.log(event)
+// call api from: https://newsapi.org/
+
+
+const HeaderOnClickedHandler = () => {
+    let path = "https://newsapi.org/v2/top-headlines?country=us&apiKey="
+    let url = path + document.getElementById("api_key").value
+    let names = document.getElementById("test")
+
+    fetch(url)
+        .then(data => {
+            return data.json()
+        })
+        .then(data => {
+            for (let atk of data.articles) {
+                let news = document.createElement('h3');
+                news.innerHTML = `${atk.source.name}`
+                names.appendChild(news)
+                // console.log(news)
+            }
+        })
+        .catch(err => console.log(err))
+
 }
